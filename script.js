1f7ae09c8ea943d9d16ce7af7d4be86a5da7d2e7
Pade venkata ramprasad <128288202+ramprasad-13@users.noreply.github.com>
2025-07-23 18:30:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+	
+    let ropeLengths=[...arr]
+    let Minimumcost=0;
+    while(ropeLengths.length>1){
+        ropeLengths.sort((a,b)=>{return a-b});
+        let first = ropeLengths.shift(0)
+        let second = ropeLengths.shift(1)
+        let cost =first+second
+        Minimumcost+=cost
+        ropeLengths.push(cost)
+    }
+    return Minimumcost;
   
 }
 
